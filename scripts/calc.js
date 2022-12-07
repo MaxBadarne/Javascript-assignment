42efd0f22d1d2cb436583d602e9f2fa7628b62a1
Max <Blackruntr@gmail.com>
2022-12-07 16:03:13 +0200

    "need to add a history function"

--- scripts/calc.js
+++ scripts/calc.js
@@ -25,13 +25,54 @@ function operate(oper,firstnum,secnum){
     }
 }
 //change display functions
-function clear(){
-    document.querySelector(".h1").textContent = "  ";
+function cleardisp(){
+    document.querySelector(".h1").textContent = "";
 }
 function insert(num){
     document.querySelector(".h1").textContent =document.querySelector(".h1").textContent +num;
 }
 function readdisplay(){
     value = document.querySelector(".h1").textContent;
+    value.trim();
+    value = parseInt(value)
     return value;
+}
+function setdisp(num){
+    document.querySelector(".h1").textContent = num;
+}
+function sethistory(num){
+    document.querySelector(".h2").textContent = num;
+}
+let num1; 
+let opr;
+function savenum1(){
+    num1 = readdisplay();
+    cleardisp();
+}
+//best part, LOGIK
+/* so there should be a waiting period
+the function will have to stop at some point after taking the display number and the perator
+*/
+function takeopp(opp){
+    savenum1();
+    opr = opp;
+    cleardisp();
+}
+function equals(){
+    num2 = readdisplay();
+    switch(opr){
+        case "times":
+            res = multiply(num1,num2);
+            break;
+        case "divi":
+            res = divide(num1,num2);
+                break;
+        case "minus":
+            res = subtract(num1,num2);
+            break;
+        case "plus":
+            res = add(num1,num2);
+            break;
+    }
+    setdisp(res)
 }
